06b9d5feddfcf5f21b34e67c6fba4707d7ff15cd
Zeta-05 <vjimmy276@gmail.com>
2025-10-31 20:23:14 +0530

    Update couples.py

--- BrandrdXMusic/plugins/tools/couples.py
+++ BrandrdXMusic/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from BrandrdXMusic.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="ᴍʏ ᴄᴜᴛᴇ ᴅᴇᴠᴇʟᴏᴘᴇʀ  🥀",
-            url=f"https://t.me/BRANDED_PAID_CC",
+            text="ᴅᴇᴠᴇʟᴏᴘᴇʀ",
+            url=f"https://t.me/Real_Dazai",
         ),
     ],
 ]
